9f86bd35c4dfda74a5f577104124f29fa7715ac8
Abhishek6116 <129516242+Abhishek6116@users.noreply.github.com>
2024-12-18 10:22:57 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log("hello abhi")
+const userId=23342;
+
+console.table([userId])
+let a=10;
+if (a===10) {
+console.log(`value of a is ${a}`)
+    
+} else {
+    console.log("value is not equal to 10")
+}
